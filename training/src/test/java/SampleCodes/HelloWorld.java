505a9c81273ee0c71cdad4ded09ae6a73622a2ff
learneasyautomation <learneasyautomation@gmail.com>
2021-08-06 17:26:36 +0530

    modified file commited

--- training/src/test/java/SampleCodes/HelloWorld.java
+++ training/src/test/java/SampleCodes/HelloWorld.java
@@ -3,7 +3,7 @@ package SampleCodes;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World modified");
 	}
 
 }
